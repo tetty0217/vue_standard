cc8748ac838f888aedb5da6004034cfc8b63b7c1
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-06-01 18:21:56 +0900

    added tutorial vue

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,23 +1,58 @@
 <template>
-	<p>
-		{{ greeting }} World!
-	</p>
+    <div>
+        <div>
+            <h2>テキストバインディング</h2>
+            <p>{{ message }}</p>
+        </div>
+        <div>
+            <h2>v-for</h2>
+            <ol>
+                <li v-for="item in list">{{ item }}</li>
+            </ol>
+        </div>
+        <div>
+            <h2>v-on:click</h2>
+            <button @click="handleClick">Click</button>
+        </div>
+        <div>
+            <h2>v-model</h2>
+            <input v-model="outMessage">
+            <p>{{outMessage}}</p>
+        </div>
+        <div>
+            <h2>v-if</h2>
+            <button v-on:click="show1=!show1">Switch</button>
+            <p v-if="show1">Hello Vue.js!</p>
+        </div>
+        <div>
+            <h2>v-if</h2>
+            <button v-on:click="show2=!show2">Switch</button>
+            <transition>
+                <p v-if="show2" class="transition">Hello Vue.js!</p>
+            </transition>
+        </div>
+    </div>
 </template>
-
 <script>
-	module.exports = {
-		data: function () {
-			return {
-				greeting: 'Parcel + Vue',
-			}
-		}
-	}
+	export default {
+	    name: "App",
+        data: function () {
+            return {
+                message: 'Hello Vue.js',
+                list: ['りんご', 'ばなな', 'いちご'],
+                outMessage: 'Initial message',
+                show1: true,
+                show2: true
+            }
+        },
+        methods: {
+            handleClick() {
+                alert("through!!!")
+            }
+        }
+    }
 </script>
-
 <style scoped>
-	p {
-		font-size: 2em;
-		text-align: center;
-		color: #2e9afe;
-	}
+
 </style>
+
