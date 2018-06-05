7f1367c67d9bec796d059f8d663f2d02d87e41bd
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-06-05 14:56:40 +0900

    added ch 2

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -12,7 +12,7 @@
         </div>
         <div>
             <h2>v-on:click</h2>
-            <button @click="handleClick">Click</button>
+            <button v-on:click="handleClick">Alert</button>
         </div>
         <div>
             <h2>v-model</h2>
@@ -31,6 +31,36 @@
                 <p v-if="show2" class="transition">Hello Vue.js!</p>
             </transition>
         </div>
+        <div>
+            <h2>v-bind</h2>
+            <input type="text" v-bind:value="bindMessage">
+        </div>
+        <div>
+            <h2>counter(v-on + method)</h2>
+            <p>{{ count }} clicked!!</p>
+            <button @click="increment">+</button>
+        </div>
+        <div>
+            <h2>v-bind for class and style</h2>
+            <p v-bind:class="{'bind-text': isClass}">binded class</p>
+            <p v-bind:style="{'color': bindStyle}">binded style</p>
+            <small v-bind:class="{'point-text': true}">sets vue attribute is <strong>win</strong></small>
+        </div>
+        <div>
+            <h2>data can sets object</h2>
+            <p v-bind:style="classObject">texttext</p>
+        </div>
+        <div>
+            <h2>v-bind image</h2>
+            <img v-bind="bindImage" v-bind:alt="'top_'+bindImage.alt">
+        </div>
+        <div>
+            <h2>binding svg</h2>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <circle cx="100" cy="75" v-bind:r="radius" fill="lightpink" />
+            </svg>
+            <input type="range" min="0" max="100" v-model="radius">
+        </div>
     </div>
 </template>
 <script>
@@ -38,16 +68,38 @@
 	    name: "App",
         data: function () {
             return {
+                //this properties is reactive
                 message: 'Hello Vue.js',
                 list: ['りんご', 'ばなな', 'いちご'],
                 outMessage: 'Initial message',
+                bindMessage: "Completed bind!",
+                contentMessage: "prop text",
                 show1: true,
-                show2: true
+                show2: true,
+                count: 0,
+                isClass: true,
+                bindStyle: 'red',
+                radius: 50,
+                classObject: {
+                    color: 'red',
+                    fontWeight: 'bold',
+                },
+                bindImage: {
+                    id: 1,
+                    src: 'http://via.placeholder.com/350x150',
+                    alt: "image_1",
+                }
             }
+        },
+        mounted: function(){
+            this.scroll = 100
         },
         methods: {
             handleClick() {
                 alert("through!!!")
+            },
+            increment() {
+                this.count += 1
             }
         }
     }
